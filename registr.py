d5ac9200068a760722cc603d9ed241dba7496ed1
mariambenali <miriam.bena@gmail.com>
2024-10-23 22:32:07 +0100

    Registration-firstCommit

---
+++ registr.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk ()
+root.geometry =("500x300")
+
+
+Label(root, text="Student Management System", font="arial 15 bold").grid(row=0, column=3)
+
+
+
+root.mainloop()
